8ab9daa601d50a5ad8483bb8e1d2917a79716f9e
supsupsuperstar <43098395+supsupsuperstar@users.noreply.github.com>
2022-05-02 01:54:53 +0800

    Update diy-part1.sh

--- Boos4721/diy-part1.sh
+++ Boos4721/diy-part1.sh
@@ -13,15 +13,14 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
 # 下载新主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
+#git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
 
 # packages
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/supsupsuperstar/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # 删除旧主题
 rm -rf package/lean/luci-theme-argon
+rm -rf ./package/lean/autocore
