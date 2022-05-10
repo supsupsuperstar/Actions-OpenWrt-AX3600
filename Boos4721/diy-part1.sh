71216b16d3f5bb638b9c288b5ae84294c94e4534
supsupsuperstar <43098395+supsupsuperstar@users.noreply.github.com>
2022-05-10 23:52:55 +0800

    Update diy-part1.sh

--- Boos4721/diy-part1.sh
+++ Boos4721/diy-part1.sh
@@ -18,9 +18,12 @@
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
 
+rm -rf ./package/lean/autocore
+git clone -b boos https://github.com/supsupsuperstar/autocore.git package/lean/autocore
+
 # packages
 sed -i '$a src-git kenzo https://github.com/supsupsuperstar/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # 删除旧主题
 rm -rf package/lean/luci-theme-argon
-rm -rf ./package/lean/autocore
+
