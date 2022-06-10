3e82d11e544e330aba644a7da9d61c40df30a115
supsupsuperstar <43098395+supsupsuperstar@users.noreply.github.com>
2022-06-10 20:46:25 +0800

    Update diy-part1.sh

--- Boos4721/diy-part1.sh
+++ Boos4721/diy-part1.sh
@@ -12,7 +12,8 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+# 删除旧主题
+rm -rf package/lean/luci-theme-argon
 
 # 下载新主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
@@ -23,6 +24,5 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 # packages
 #sed -i '$a src-git kenzo https://github.com/supsupsuperstar/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# 删除旧主题
-rm -rf package/lean/luci-theme-argon
+
 
